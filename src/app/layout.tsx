75658442c24e54c9ac76b5c188ec705208bd4ab1
elitefutbot <elitefutbotfc@gmail.com>
2026-08-14 12:19:56 +0530

    feat(analytics): add Microsoft Clarity tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -150,6 +150,16 @@ export default function RootLayout({
             gtag('config', 'G-224X1P08Z4');
           `
         }} />
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "y24lqc637w");
+          `
+        }} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://www.youtube-nocookie.com" />
